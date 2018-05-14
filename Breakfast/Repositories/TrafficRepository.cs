c13b1aded8afcc8058c5c15671e07a9921e3fe88
Unknown <mrcrozier@gmail.com>
2018-05-14 15:49:33 -0400

    Real sample repository pattern
    I forgot to save my changes hah

--- Breakfast/Repositories/TrafficRepository.cs
+++ Breakfast/Repositories/TrafficRepository.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Configuration;
 using System.Text;
 using System.Threading.Tasks;
+using RepositoryInterfaces;
 
 namespace Repositories
 {
-    public class Class1
+    public class TrafficRepository : ITrafficRepository
     {
+        //TODO put db context here
+        //TODO put constructor that takes in db context here
+
+        //example of how to do the crud operations. This is read
+//        public IQueryable<Model> GetAll()
+//        {
+//            return dbContext.Models;
+//        }
     }
 }
