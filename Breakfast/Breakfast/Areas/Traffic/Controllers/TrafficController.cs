a642ea98734ac50dd2e915286beb2a6e8012c59a
Unknown <mrcrozier@gmail.com>
2018-05-15 16:43:51 -0400

    Added basic maps integration, controller
    tweaked css for map size. apparently added shared views? that'll be a merge conflict probably, just delete mine its autogenerated

---
+++ Breakfast/Breakfast/Areas/Traffic/Controllers/TrafficController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Breakfast.Areas.Traffic.Controllers
+{
+    public class TrafficController : Controller
+    {
+        // GET: Traffic/Traffic
+        public ActionResult ViewMap()
+        {
+            string apiKey = null;
+            try
+            {
+                apiKey = System.IO.File.ReadAllText(System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory.ToString(), "mapskey.txt"));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Shit failed");
+            }
+
+            ViewBag.APIKey = apiKey;
+            return View();
+        }
+    }
+}
