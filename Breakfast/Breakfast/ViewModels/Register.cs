5295bdba3dcf52e0bc47cf9c7a1736db12334660
Sebastian Majko <smajko31@gmail.com>
2018-05-16 19:18:50 -0400

    add register model

---
+++ Breakfast/Breakfast/ViewModels/Register.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Breakfast.ViewModels
+{
+    public class Register
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.PostalCode)]
+        public string Zipcode { get; set; }
+
+        public string Address { get; set; }
+
+        public string WorkAddress { get; set; }
+    }
+}
