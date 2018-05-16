5ed47a7558fac65a2314c270bac78f9480afc0bf
Unknown <mrcrozier@gmail.com>
2018-05-16 10:50:39 -0400

    Shell for duration to work added

--- Breakfast/Breakfast/Areas/Traffic/Controllers/TrafficController.cs
+++ Breakfast/Breakfast/Areas/Traffic/Controllers/TrafficController.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 
 namespace Breakfast.Areas.Traffic.Controllers
 {
-    public class TrafficController : Controller
+    public class TrafficController : AsyncController
     {
         // GET: Traffic/Traffic
-        public ActionResult ViewMap()
+        public async Task<ActionResult> ViewMap()
         {
             string apiKey = null;
             try
@@ -20,7 +24,24 @@ namespace Breakfast.Areas.Traffic.Controllers
             {
                 Console.WriteLine("Shit failed");
             }
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(@"https://maps.googleapis.com/maps/api/distancematrix/json?origins=Bellarmine+Newman+Hall&destinations=MacDintons+SoHo&mode=driving&key=AIzaSyD0FSkqNKs7dZ30P_yDVwmFQglHpazFCgQ=");
+            string insert = null;
+            if (response.IsSuccessStatusCode)
+            {
+               insert = await response.Content.ReadAsStringAsync();
+            }
 
+            Match match = Regex.Match(insert, "[0-9]+(?= mins)");
+            if (match.Success)
+            {
+                var result = match.Captures[0].Value;
+                ViewBag.TimeToWork = result;
+            }
+
+            int minuteIndex = insert.IndexOf("mins");
+            ViewBag.Insert = insert;
+            
             ViewBag.APIKey = apiKey;
             return View();
         }
