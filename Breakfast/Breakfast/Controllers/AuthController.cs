18aec9c99c4b23888b3c5ba41367259e7c97440c
Sebastian Majko <smajko31@gmail.com>
2018-05-16 12:01:09 -0400

    add viewmodel, sample view, controller for login

---
+++ Breakfast/Breakfast/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Breakfast.Controllers
+{
+    [AllowAnonymous]
+    public class AuthController : Controller
+    {
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+    }
+}
