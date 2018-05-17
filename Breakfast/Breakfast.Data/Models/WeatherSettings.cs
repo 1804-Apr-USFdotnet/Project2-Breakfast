02d4e1e89f94400507a4a25d9a636e614ee13600
Sebastian Majko <smajko31@gmail.com>
2018-05-17 09:24:01 -0400

    create weather settings model

--- Breakfast/Breakfast.Data/Models/WeatherSettings.cs
+++ Breakfast/Breakfast.Data/Models/WeatherSettings.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Breakfast.Data.Models
 {
     class WeatherSettings
     {
+        [ForeignKey("SettingsTable")]
+        public int Fk_WeatherId { get; set; }
+
+        public bool Enabled { get; set; }
+        public bool Farenheit { get; set; }
+        public bool WindSpeed { get; set; }
+        public bool Humidity { get; set; }
+        public bool Cloudiness { get; set; }
+        public string Location { get; set; }
+
+        public SettingsTable SettingsTable { get; set; }
     }
 }
