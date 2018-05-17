f4191f3dc37b9ae7383c66a2d12ca67c8a6d20fb
Sebastian Majko <smajko31@gmail.com>
2018-05-17 09:28:02 -0400

    create dbcontext

---
+++ Breakfast/Breakfast.Data/SettingsContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Breakfast.Data
+{
+    class SettingsContext : DbContext
+    {
+        public SettingsContext() : base("SettingsContext")
+        {
+        }
+
+        public DbSet<Models.SettingsTable> Settings { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
