b0507d4c20f21c59756b34e0ff4724de2c94c901
Sebastian Majko <smajko31@gmail.com>
2018-05-18 13:43:30 -0400

    add domain objects for current and forecast weather

---
+++ BreakfastApi/Breakfast.Business/Weather/Models/CurrentWeather.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Breakfast.Business.Weather.Models
+{
+    class CurrentWeather
+    {
+        public string city { get; private set; }
+        public string country { get; private set; }
+        public string description { get; private set; }
+        public double windSpeed { get; private set; }
+        public int temperature { get; private set; }
+        public char temperatureType { get; private set; } = 'F';
+        public int humidity { get; private set; }
+        public int cloudiness { get; private set; }
+        public string condition { get; set; } = "09d";
+    }
+}
---
+++ BreakfastApi/Breakfast.Business/Weather/Models/ForecastWeather.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Breakfast.Business.Weather.Models
+{
+    class ForecastWeather
+    {
+        public int tempMin { get; set; }
+        public int tempMax { get; set; }
+        public string day { get; set; }
+        public string condition { get; set; }
+    }
+}
