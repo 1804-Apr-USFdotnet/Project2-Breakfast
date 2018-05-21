8973471181d46c116a685ea55bc77beed210926e
Austin A <austin.ascura@gmail.com>
2018-05-21 16:39:18 -0400

    minor comment removals

--- BreakfastApi/Breakfast.Data/Models/NewsSettings.cs
+++ BreakfastApi/Breakfast.Data/Models/NewsSettings.cs
@@ -10,10 +10,9 @@ namespace Breakfast.Data.Models
         [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
         public int Pk_NewsId { get; set; }
         public bool Enabled { get; set; }
-        // TODO: add news specific settings
-
+       
         public string Queries;
-        public string Sources; // questioning implementation here
+        public string Sources;
         public string Domains;
         public Nullable<DateTime> OldestDate;
         public Nullable<DateTime> NewestDate;
