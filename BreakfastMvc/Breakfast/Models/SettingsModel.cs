69ad745a51d3bbc35cad5052c7a976774b5dbdbe
Sebastian Majko <smajko31@gmail.com>
2018-05-21 16:23:45 -0400

    add method to consume service (get all settings)

---
+++ BreakfastMvc/Breakfast/Models/SettingsModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace Breakfast.Models
+{
+    public class SettingsModel
+    {
+        string uri = "http://ec2-18-191-47-17.us-east-2.compute.amazonaws.com/Breakfast.Service_deploy/";
+        
+        public void GetSettings(string userId)
+        {
+            HttpWebRequest apiRequest = WebRequest.Create(uri + "api/settings/get/" + userId) as HttpWebRequest;
+            string apiResponse = "";
+            using (HttpWebResponse response = apiRequest.GetResponse() as HttpWebResponse)
+            {
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                apiResponse = reader.ReadToEnd();
+            }
+        }
+    }
+}
