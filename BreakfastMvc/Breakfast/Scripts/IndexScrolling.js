a0b62ad32bc2881f2fb163981c2212d47f0e8fd4
Sebastian Majko <smajko31@gmail.com>
2018-05-21 14:27:25 -0400

    fix sidebar scrolling for main page

--- BreakfastMvc/Breakfast/Scripts/IndexScrolling.js
+++ BreakfastMvc/Breakfast/Scripts/IndexScrolling.js
@@ -1,16 +1,15 @@
 ﻿$( document ).ready( function() {
   // scrollTo('.Sidebar-links')		;
   scrollTo();
-  scrollToTop();
 });
 
 function scrollTo () {
-  $('.Sidebar a').click(function(e) {
+    $('.Sidebar-navItem a').click(function(e) {
       e.preventDefault();
-      $('.Sidebar a').removeClass('active');
+        $('.Sidebar-navItem a').removeClass('active');
       $(this).addClass('active');
     
-     var distanceTopToSection = $( '#' +  $(this).data('target')).offset().top;
+     var distanceTopToSection = $($(this).data('target')).offset().top;
     		 $( 'body, html' ).animate({scrollTop:distanceTopToSection }, 'slow');
   });
 }
