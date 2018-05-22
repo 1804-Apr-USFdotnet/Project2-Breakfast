a2757c0145f9c12ad189445cef435a4d4b37e745
Sebastian Majko <smajko31@gmail.com>
2018-05-22 09:59:01 -0400

    add script to toggle dark mode

---
+++ BreakfastMvc/Breakfast/Scripts/DarkMode.js
@@ -0,0 +1,9 @@
+﻿$('#mode').change(function () {
+
+    if ($(this).prop('checked')) {
+        $('body').addClass('dark-mode');
+    }
+    else {
+        $('body').removeClass('dark-mode');
+    }
+});
