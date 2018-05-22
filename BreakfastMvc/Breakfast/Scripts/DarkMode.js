71d8793ae2acf3683823fab0e5c141ed9ddedbde
Sebastian Majko <smajko31@gmail.com>
2018-05-22 10:57:50 -0400

    fix dark mode syling

--- BreakfastMvc/Breakfast/Scripts/DarkMode.js
+++ BreakfastMvc/Breakfast/Scripts/DarkMode.js
@@ -1,8 +1,12 @@
-﻿$('#mode').change(function () {
-    if ($(this).prop('checked')) {
-        $('.wrapper').addClass('dark-mode');
-    }
-    else {
-        $('.wrapper').removeClass('dark-mode');
-    }
+﻿$(document).ready(function () {
+    $('#mode').on('change', function () {
+        if ($(this).prop('checked')) {
+            $('.wrapper').addClass('dark-mode');
+            $('.wrapper').removeClass('light-mode');
+        }
+        else {
+            $('.wrapper').removeClass('dark-mode');
+            $('.wrapper').addClass('light-mode');
+        }
+    });
 });
