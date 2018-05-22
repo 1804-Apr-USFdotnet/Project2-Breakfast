de9fdda3ca23328584c0d9658d799c4f3a7aad16
Sebastian Majko <smajko31@gmail.com>
2018-05-22 11:08:37 -0400

    fix text color for dark mode

--- BreakfastMvc/Breakfast/Scripts/DarkMode.js
+++ BreakfastMvc/Breakfast/Scripts/DarkMode.js
@@ -3,10 +3,15 @@
         if ($(this).prop('checked')) {
             $('.wrapper').addClass('dark-mode');
             $('.wrapper').removeClass('light-mode');
+            $('*').addClass('dark-mode-text');
+            $('*').removeClass('light-mode-text');
+
         }
         else {
             $('.wrapper').removeClass('dark-mode');
             $('.wrapper').addClass('light-mode');
+            $('*').removeClass('dark-mode-text');
+            $('*').addClass('light-mode-text');
         }
     });
 });
