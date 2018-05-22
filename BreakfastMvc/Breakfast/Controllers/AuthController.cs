ab004f6299fc038631203809376e0f559a17b363
Sebastian Majko <smajko31@gmail.com>
2018-05-22 15:04:39 -0400

    fix action link for login on sidebar

--- BreakfastMvc/Breakfast/Controllers/AuthController.cs
+++ BreakfastMvc/Breakfast/Controllers/AuthController.cs
@@ -2,9 +2,6 @@
 using Breakfast.ViewModels;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
