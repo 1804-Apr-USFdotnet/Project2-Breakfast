7cff05d37984ad65f265593fe857b84766025380
Sebastian Majko <smajko31@gmail.com>
2018-05-23 21:52:40 -0400

    finish settings page structure

---
+++ BreakfastMvc/Breakfast/Scripts/tabs.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    var tabs = $(".tabs li a");
+    tabs.click(function () {
+        var content = this.hash.replace('/', '');
+        tabs.removeClass("active");
+        $(this).addClass("active");
+        $("#content").find('p').hide();
+        $("#content").find('.wrapper-settings').hide();
+        //$(content).fadeIn(200);
+        $(content).show();
+        $(content).find('.wrapper-settings').fadeIn(200);
+
+    });
+});
