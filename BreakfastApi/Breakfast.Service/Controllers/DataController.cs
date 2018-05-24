9d1c82774324d61b4120a312b446ed0603682d06
Sebastian Majko <smajko31@gmail.com>
2018-05-24 09:33:45 -0400

    change getting api data from mvc to service layer

---
+++ BreakfastApi/Breakfast.Service/Controllers/DataController.cs
@@ -0,0 +1,24 @@
+﻿using Breakfast.Business.Weather;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+
+namespace Breakfast.Service.Controllers
+{
+    public class DataController : ApiController
+    {
+        [HttpGet]
+        [ResponseType(typeof(string))]
+        [Route("api/settings/get/{userId}")]
+        public IHttpActionResult WeatherData(string zipcode)
+        {
+            try { return Ok(OpenWeatherMapApi.GetResponse(zipcode)); }
+            catch { return InternalServerError(); }
+        }
+    }
+}
