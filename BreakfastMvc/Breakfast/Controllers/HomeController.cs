7e7ee2b55331c464183535da66da72f617dbf017
Sebastian Majko <smajko31@gmail.com>
2018-05-24 14:11:32 -0400

    tie identity to data

--- BreakfastMvc/Breakfast/Controllers/HomeController.cs
+++ BreakfastMvc/Breakfast/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
 ﻿using Breakfast.Areas.Weather.Models;
 using Breakfast.Models;
 using Breakfast.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 
 namespace Breakfast.Controllers
@@ -11,12 +15,15 @@ namespace Breakfast.Controllers
         {
             if (User.Identity.IsAuthenticated)
             {
+                string currentUserId = User.Identity.GetUserId();
+                AppDbContext db = new AppDbContext();
+                AppUser currentUser = db.Users.FirstOrDefault(x => x.Id == currentUserId);
                 Data data = new Data()
                 {
                     // get settings for current user
                     settings = new SettingsModel().GetSettings(User.Identity.Name),
                     // get weather data for current user
-                    weatherData = new OpenWeatherMap("10305")
+                    weatherData = new OpenWeatherMap(currentUser.zipcode)
                     // get traffic data for current user
                     // TODO
                     // get news data for current user
