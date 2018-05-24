9cb02eb5be73c0d1de4e4f2bf80c267b08965aff
Sebastian Majko <smajko31@gmail.com>
2018-05-24 13:17:30 -0400

    fix conversion to new json response

--- BreakfastMvc/Breakfast/Areas/Weather/JsonResponseHelpers/WeatherData.cs
+++ BreakfastMvc/Breakfast/Areas/Weather/JsonResponseHelpers/WeatherData.cs
@@ -7,5 +7,26 @@ namespace Breakfast.Areas.Weather.JsonResponseHelpers
 {
     public class WeatherData
     {
+        public class ForecastDay
+        {
+            public int tempMin { get; set; }
+            public int tempMax { get; set; }
+            public string day { get; set; }
+            public string condition { get; set; }
+        }
+
+        public class RootObject
+        {
+            public string city { get; set; }
+            public string country { get; set; }
+            public string description { get; set; }
+            public double windSpeed { get; set; }
+            public int temperature { get; set; }
+            public string temperatureType { get; set; }
+            public int humidity { get; set; }
+            public int cloudiness { get; set; }
+            public string condition { get; set; }
+            public List<ForecastDay> forecastDays { get; set; }
+        }
     }
 }
--- BreakfastMvc/Breakfast/Areas/Weather/Models/OpenWeatherMap.cs
+++ BreakfastMvc/Breakfast/Areas/Weather/Models/OpenWeatherMap.cs
@@ -34,42 +34,7 @@ namespace Breakfast.Areas.Weather.Models
 
         public void GetResponse(string zipcode)
         {
-            string apiKey = "6e0c425a741c67ae35eda9b8812c60b8";
-            HttpWebRequest apiRequest = WebRequest.Create("http://api.openweathermap.org/data/2.5/weather?zip=" + zipcode.ToString() + "&appid=" + apiKey + "&units=imperial") as HttpWebRequest;
-            HttpWebRequest apiRequest2 = WebRequest.Create("http://ec2-18-188-45-20.us-east-2.compute.amazonaws.com/Breakfast.Service_deploy/" + "api/weather/get/" + zipcode) as HttpWebRequest;
-
-            string apiResponse = "";
-            string apiResponse2 = "";
-            using (HttpWebResponse response = apiRequest.GetResponse() as HttpWebResponse)
-            {
-                StreamReader reader = new StreamReader(response.GetResponseStream());
-                apiResponse = reader.ReadToEnd();
-            }
-
-            using (HttpWebResponse response2 = apiRequest2.GetResponse() as HttpWebResponse)
-            {
-                StreamReader reader = new StreamReader(response2.GetResponseStream());
-                apiResponse2 = reader.ReadToEnd();
-            }
-
-            JsonResponseHelpers.CurrentWeather.ResponseWeather rootObject = JsonConvert.DeserializeObject<JsonResponseHelpers.CurrentWeather.ResponseWeather>(apiResponse);
-
-            city = rootObject.name;
-            country = rootObject.sys.country;
-            description = rootObject.weather[0].description;
-            windSpeed = rootObject.wind.speed;
-            temperature = (int)rootObject.main.temp;
-            humidity = rootObject.main.humidity;
-            cloudiness = rootObject.clouds.all;
-            condition = rootObject.weather[0].icon;
-
-            GetForecast(zipcode);
-        }
-
-        private void GetForecast(string zipcode)
-        {
-            string apiKey = "6e0c425a741c67ae35eda9b8812c60b8";
-            HttpWebRequest apiRequest = WebRequest.Create("http://api.openweathermap.org/data/2.5/forecast?zip=" + zipcode.ToString() + "&appid=" + apiKey + "&units=imperial") as HttpWebRequest;
+            HttpWebRequest apiRequest = WebRequest.Create("http://ec2-18-188-45-20.us-east-2.compute.amazonaws.com/Breakfast.Service_deploy/" + "api/weather/get/" + zipcode) as HttpWebRequest;
 
             string apiResponse = "";
             using (HttpWebResponse response = apiRequest.GetResponse() as HttpWebResponse)
@@ -78,36 +43,29 @@ namespace Breakfast.Areas.Weather.Models
                 apiResponse = reader.ReadToEnd();
             }
 
-            JsonResponseHelpers.ForecastWeather.ResponseWeather rootObject = JsonConvert.DeserializeObject<JsonResponseHelpers.ForecastWeather.ResponseWeather>(apiResponse);
+            JsonResponseHelpers.WeatherData.RootObject rootObject = JsonConvert.DeserializeObject<JsonResponseHelpers.WeatherData.RootObject>(apiResponse);
 
-            for (int i = 0; i < 5; i++)
+            city = rootObject.city;
+            country = rootObject.country;
+            description = rootObject.description;
+            windSpeed = rootObject.windSpeed;
+            temperature = rootObject.temperature;
+            humidity = rootObject.humidity;
+            cloudiness = rootObject.cloudiness;
+            condition = rootObject.condition;
+
+            int curr = 0;
+            foreach (var item in rootObject.forecastDays)
             {
-                forecastDays[i] = new ForecastDay();
-
-                int year, month, day;
-                // Get date parts out of json date format (yyyy-mm-dd)
-                Int32.TryParse(rootObject.list[i*8].dt_txt.Substring(0, 4), out year);
-                Int32.TryParse(rootObject.list[i*8].dt_txt.Substring(5, 2), out month);
-                Int32.TryParse(rootObject.list[i*8].dt_txt.Substring(8, 2), out day);
-
-                DateTime dateValue = new DateTime(year, month, day);
-
-                // Access nth day in json array, [0] = day 1, [8] = day 2, [16] = day 3, etc.
-                forecastDays[i].day = dateValue.ToString("ddd");
-                forecastDays[i].condition = rootObject.list[i*8].weather[0].icon;
-                forecastDays[i].tempMax = (int)rootObject.list[i*8].main.temp_max;
-                forecastDays[i].tempMin = (int)rootObject.list[i*8].main.temp_min;
-
-                // Go through entire day to get max and min temperature of the day
-                for (int j = 0; j < 8; j++)
-                {
-                    if ((i * 8 + j) >= rootObject.list.Count)
-                        break;
-                    if (forecastDays[i].tempMax < rootObject.list[i * 8 + j].main.temp_max)
-                        forecastDays[i].tempMax = (int)rootObject.list[i * 8 + j].main.temp_max;
-                    if (forecastDays[i].tempMin > rootObject.list[i * 8 + j].main.temp_min)
-                        forecastDays[i].tempMin = (int)rootObject.list[i * 8 + j].main.temp_min;
-                }
+                if (curr < 5)
+                    forecastDays[curr] = new ForecastDay
+                    {
+                        condition = item.condition,
+                        day = item.day,
+                        tempMax = item.tempMax,
+                        tempMin = item.tempMin
+                    };
+                curr++;
             }
         }
     }
