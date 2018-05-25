f4ec6c152644868022d73780faa1d7b953fe044b
Sebastian Majko <smajko31@gmail.com>
2018-05-24 20:49:12 -0400

    add unit test for data to business model conversion

--- BreakfastApi/Breakfast.Test/TestConvert.cs
+++ BreakfastApi/Breakfast.Test/TestConvert.cs
@@ -33,5 +33,31 @@ namespace Breakfast.Test
             Assert.AreEqual(wsData.Cloudiness, param3);
             Assert.AreEqual(wsData.WindSpeed, param4);
         }
+
+        [TestMethod]
+        public void WeatherDataToBusiness()
+        {
+            //Arrange
+            Data.Models.WeatherSettings wsData = new Data.Models.WeatherSettings()
+            {
+                Enabled = false,
+                Humidity = true,
+                Cloudiness = true,
+                WindSpeed = false
+            };
+
+            //Act
+            WeatherSettings ws = (WeatherSettings)wsData;
+            bool param1 = false;
+            bool param2 = true;
+            bool param3 = true;
+            bool param4 = false;
+
+            //Assert
+            Assert.AreEqual(ws.enabled, param1);
+            Assert.AreEqual(ws.humidity, param2);
+            Assert.AreEqual(ws.cloudiness, param3);
+            Assert.AreEqual(ws.windSpeed, param4);
+        }
     }
 }
