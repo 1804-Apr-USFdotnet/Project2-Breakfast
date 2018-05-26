46d414eee51ec4243d09996ab377ba8374cc18a0
Sebastian Majko <smajko31@gmail.com>
2018-05-26 19:10:46 -0400

    add debugger (chrome) config

--- BreakfastAngular/src/app/weather/weather.component.ts
+++ BreakfastAngular/src/app/weather/weather.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { WeatherData } from '../models/WeatherData';
+import { ForecastDay } from '../models/ForecastDay';
 import { WeatherService } from '../weather.service';
 
 @Component({
@@ -11,10 +12,9 @@ import { WeatherService } from '../weather.service';
 export class WeatherComponent implements OnInit {
   weather: WeatherData
 
-  constructor(
-    private route: ActivatedRoute,
-    private weatherService: WeatherService
-  ) { }
+  constructor(private route: ActivatedRoute, private weatherService: WeatherService) { 
+    // this.weather.forecastDays = new Array<ForecastDay>(new ForecastDay({tempMin: 1, tempMax: 2}));
+  }
 
   ngOnInit() {
     //var zipcode = this.route.snapshot.paramMap.get("zipcode");
