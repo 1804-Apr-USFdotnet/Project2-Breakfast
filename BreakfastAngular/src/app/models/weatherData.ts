b0c6c6b48b999f9acb4e08bc07c03a4813397921
Sebastian Majko <smajko31@gmail.com>
2018-05-26 18:41:02 -0400

    create two classes instead of nested

--- BreakfastAngular/src/app/models/weatherData.ts
+++ BreakfastAngular/src/app/models/weatherData.ts
@@ -1,3 +1,5 @@
+import { ForecastDay } from '../models/ForecastDay';
+
 export class WeatherData
 {
   city: string;
@@ -9,36 +11,49 @@ export class WeatherData
   humidity: number;
   cloudiness: number;
   condition: string;
-  forecastDays: [
-    {
-      tempMin: number;
-      tempMax: number;
-      day: string;
-      condition: string;
-    },
-    {
-      tempMin: number;
-      tempMax: number;
-      day: string;
-      condition: string;
-    },
-    {
-      tempMin: number;
-      tempMax: number;
-      day: string;
-      condition: string;
-    },
-    {
-      tempMin: number;
-      tempMax: number;
-      day: string;
-      condition: string;
-    },
-    {
-      tempMin: number;
-      tempMax: number;
-      day: string;
-      condition: string;
-    }
-  ]
+  forecastDays: ForecastDay[];
 }
+// export class WeatherData
+// {
+//   city: string;
+//   country: string;
+//   description: string;
+//   windSpeed: number;
+//   temperature: number;
+//   temperatureType: string;
+//   humidity: number;
+//   cloudiness: number;
+//   condition: string;
+//   forecastDays: [
+//     {
+//       tempMin: number;
+//       tempMax: number;
+//       day: string;
+//       condition: string;
+//     },
+//     {
+//       tempMin: number;
+//       tempMax: number;
+//       day: string;
+//       condition: string;
+//     },
+//     {
+//       tempMin: number;
+//       tempMax: number;
+//       day: string;
+//       condition: string;
+//     },
+//     {
+//       tempMin: number;
+//       tempMax: number;
+//       day: string;
+//       condition: string;
+//     },
+//     {
+//       tempMin: number;
+//       tempMax: number;
+//       day: string;
+//       condition: string;
+//     }
+//   ]
+// }
