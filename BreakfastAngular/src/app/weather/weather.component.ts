5d9b18ca17c42e248ea6be00d4f8e92147e4406f
Sebastian Majko <smajko31@gmail.com>
2018-05-27 11:43:23 -0400

    resolve merge conflict

--- BreakfastAngular/src/app/weather/weather.component.ts
+++ BreakfastAngular/src/app/weather/weather.component.ts
@@ -18,69 +18,16 @@ export class WeatherComponent implements OnInit {
   }
 
   ngOnInit() {
-<<<<<<< Updated upstream
-    //var zipcode = this.route.snapshot.paramMap.get("zipcode");
     this.searchZip = "33617";
     this.getWeather();
-    // this.weatherService.getWeatherByZipcode(
-    //   zipcode, 
-    //   (response) => {this.weather = response, this.getIcon(this.weather.condition)}
-    // );
-=======
-    var zipcode = "33617";
-    this.getWeather();
->>>>>>> Stashed changes
   }
 
   getWeather() {
     this.weatherService.getWeatherByZipcode(
-<<<<<<< Updated upstream
         this.searchZip, 
         (response) => {
-            this.weather = response, 
-            this.weather.condition = this.getIcon(this.weather.condition),
-            this.weather.forecastDays.forEach(function(part, index, forecastArray) {
-                forecastArray[index] = this.getIcon(forecastArray[index]);
-          });
+            this.weather = response
         }
     );
   }
-
-  getIcon(icon: string) {
-
-    if (Number(icon.substring(0,2)) > 3){
-      icon = icon.substring(0,2);
-    }
-
-    switch (icon) {
-      case "01d":
-          return "wi wi-day-sunny";
-      case "02d":
-          return "wi wi-day-sunny-overcast";
-      case "01n":
-          return "wi wi-night-clear";
-      case "02n":
-          return "wi wi-night-partly-cloudy";          
-      case "03":
-          return "wi wi-cloud";   
-      case "04":
-          return "wi wi-cloudy";
-      case "09":
-          return "wi wi-showers";
-      case "10":
-          return "wi wi-rain";
-      case "11":
-          return "wi wi-thunderstorm";
-      case "13":
-          return "wi wi-snow";
-      case "50":
-          return "wi wi-fog";
-    }
-  }
-=======
-      this.searchZip, 
-      (response) => {this.weather = response}
-    );
-  }
->>>>>>> Stashed changes
 }
