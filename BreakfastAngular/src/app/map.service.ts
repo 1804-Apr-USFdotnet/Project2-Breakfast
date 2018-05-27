bb3aacef0454c574b1f4f2c90a4358f5ddab460d
Sebastian Majko <smajko31@gmail.com>
2018-05-27 14:48:48 -0400

    finish map service functionality

--- BreakfastAngular/src/app/map.service.ts
+++ BreakfastAngular/src/app/map.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+getMap(
+  homePlaceId : string, 
+  workPlaceId : string,
+  travelMode : string,
+  onSuccess,
+  onFail = (reason) => console.log(reason)) {
+    var url = "http://ec2-18-188-45-20.us-east-2.compute.amazonaws.com/Breakfast.Service_deploy/api/traffic/get/" 
+            + homePlaceId + "/"
+            + workPlaceId + "/"
+            + travelMode;
+    var req = this.httpClient.get(url);
+    var promise = req.toPromise();
+
+    promise.then(
+      onSuccess,
+      onFail
+    );
+  }
 }
+
