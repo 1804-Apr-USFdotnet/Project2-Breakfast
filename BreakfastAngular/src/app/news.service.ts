1cf0defdd15eef123c794c12fa7b1c405d008f4c
Sebastian Majko <smajko31@gmail.com>
2018-05-28 00:13:51 -0400

    call news rest action from service

--- BreakfastAngular/src/app/news.service.ts
+++ BreakfastAngular/src/app/news.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
 
-  constructor() { }
-}
+  constructor(private httpClient: HttpClient) { }
+
+  getArticles(
+    userId: string,
+    onSuccess,
+    onFail = (reason) => console.log(reason)) {
+      var url = "http://ec2-18-188-45-20.us-east-2.compute.amazonaws.com/Breakfast.Service_deploy/api/news/getArticles/" + userId;
+      var req = this.httpClient.get(url);
+      var promise = req.toPromise();
+
+      promise.then(
+        onSuccess,
+        onFail
+      );
+    }
+  }
