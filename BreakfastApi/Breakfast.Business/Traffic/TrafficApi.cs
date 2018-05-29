c6136b8cd8b99f18d67ec1463df87570cdc079b3
Unknown <mrcrozier@gmail.com>
2018-05-29 01:35:25 -0400

    Added new option for time api
    Updated calls accordingly

--- BreakfastApi/Breakfast.Business/Traffic/TrafficApi.cs
+++ BreakfastApi/Breakfast.Business/Traffic/TrafficApi.cs
@@ -16,16 +16,6 @@ namespace Breakfast.Business.Traffic
         {
             TravelTimeToWork tttw = new TravelTimeToWork();
             string apiKey = "AIzaSyD-Dndf6f1-0LZCgSB4zaBCqtApaSTIquo";
-//            try
-//            {
-//                apiKey = System.IO.File.ReadAllText(System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory.ToString(), "mapskey.txt"));
-//            }
-//            catch (Exception e)
-//            {
-//                Console.WriteLine("It failed, do you have mapskey.txt in the same directory of the project");
-//                tttw.Time = "API Key didnt parse correctly";
-//                return tttw;
-//            }
             
             HttpClient client = new HttpClient();
             
@@ -55,7 +45,49 @@ namespace Breakfast.Business.Traffic
             tttw.Time = "Unable to get your time to work";
             return tttw;
         }
+        public static async Task<TravelTimeToWork> GetTimeToWorkString(string home, string work, string travelMode)
+        {
+            TravelTimeToWork tttw = new TravelTimeToWork();
+            string apiKey = "AIzaSyD-Dndf6f1-0LZCgSB4zaBCqtApaSTIquo";
+            string newHome = ParseAutocomplete(home);
+            string newWork = ParseAutocomplete(work);
+            HttpClient client = new HttpClient();
 
-        
+            string url = @"https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + newHome +
+                         @"&destinations=" + newWork + @"&mode=" + travelMode + @"&key=" + apiKey;
+            string test = "test";
+            var response = await client.GetAsync(url);
+
+
+
+
+            string insert = null;
+            if (response.IsSuccessStatusCode)
+            {
+                insert = await response.Content.ReadAsStringAsync();
+            }
+
+            if (insert != null)
+            {
+                Match match = Regex.Match(insert, "[0-9]+(?= mins)");
+                if (match.Success)
+                {
+                    tttw.Time = match.Captures[0].Value;
+                    return tttw;
+                }
+            }
+            tttw.Time = "Unable to get your time to work";
+            return tttw;
+        }
+
+        private static string ParseAutocomplete(string address)
+        {
+            string result = "";
+            result = address.Replace(" ", "+");
+            result = address.Replace(", ", "+");
+            return result;
+        }
+
+
     }
 }
